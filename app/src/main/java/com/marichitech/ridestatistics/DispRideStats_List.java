9d5a7480f6cac241f1069f16d7960cc08fc6d045
kaustubh <kaustubh.kivijoshi@gmail.com>
2018-05-16 21:16:30 +0200

    Version 1.1 : + working real time charts

--- app/src/main/java/com/marichitech/ridestatistics/DispRideStats_List.java
+++ app/src/main/java/com/marichitech/ridestatistics/DispRideStats_List.java
@@ -2,6 +2,7 @@ package com.marichitech.ridestatistics;
 
 import android.content.Context;
 import android.content.pm.ActivityInfo;
+import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -11,6 +12,16 @@ import android.os.Bundle;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+import com.github.mikephil.charting.utils.Utils;
+
 public class DispRideStats_List extends AppCompatActivity implements SensorEventListener
 {
     int[] images = {R.drawable.motorcycle, R.drawable.motorcycle,R.drawable.motorcycle};
@@ -18,8 +29,10 @@ public class DispRideStats_List extends AppCompatActivity implements SensorEvent
     String[] Angle_Name = {"Azimuth", "Pitch", "Roll"};
 
     String[] Angle_values = {"0.0", "0.0", "0.0"};
-
+    float[] Angles = {0f, 0f, 0f};
     ListView lView;
+
+    ILineDataSet[] datasets = new ILineDataSet[3];
 
     ListAdapter lAdapter;
     // System sensor manager instance.
@@ -29,6 +42,11 @@ public class DispRideStats_List extends AppCompatActivity implements SensorEvent
     // sensor manager.
     private Sensor mSensorAccelerometer;
     private Sensor mSensorMagnetometer;
+
+    private LineChart mChart;
+    private Thread thread;
+    private boolean plotData = true;
+
 
     // TextViews to display current sensor values.
     private TextView mTextSensorAzimuth;
@@ -43,6 +61,13 @@ public class DispRideStats_List extends AppCompatActivity implements SensorEvent
     private float[] mAccelerometerData = new float[3];
     private float[] mMagnetometerData = new float[3];
 
+    private boolean b_init = false;
+    private float Azimuth_max = 0;
+    private float Pitch_max = 0;
+    private float Roll_max = 0;
+
+    private float[] init_values = new float[3];
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -50,9 +75,24 @@ public class DispRideStats_List extends AppCompatActivity implements SensorEvent
 
         lView = (ListView) findViewById(R.id.androidList);
 
-        lAdapter = new ListAdapter(DispRideStats_List.this, Angle_Name, Angle_values, images);
+        for (int i = 0; i < 3; i++) {
+            Utils.init(getResources());
+            LineDataSet set_temp = new LineDataSet(null,"Dynamic Data");
+            set_temp.setAxisDependency(YAxis.AxisDependency.LEFT);
+            set_temp.setLineWidth(3);
+            set_temp.setColor(Color.MAGENTA);
+            set_temp.setHighlightEnabled(false);
+            set_temp.setDrawValues(false);
+            set_temp.setDrawCircles(false);
+            set_temp.setMode(LineDataSet.Mode.CUBIC_BEZIER);
+            set_temp.setCubicIntensity(0.2f);
+            datasets[i] = set_temp;
+        }
+        //lAdapter = new ListAdapter(DispRideStats_List.this, Angle_Name, Angle_values,Angles,datasets, images);
 
-        lView.setAdapter(lAdapter);
+        //lView.setAdapter(lAdapter);
+
+        //startPlot();
         // Lock the orientation to portrait (for now)
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
@@ -102,6 +142,18 @@ public class DispRideStats_List extends AppCompatActivity implements SensorEvent
         mSensorManager.unregisterListener(this);
     }
 
+
+
+    private LineDataSet CreateSet(){
+        LineDataSet set = new LineDataSet(null,"Dynamic Data");
+        set.setAxisDependency(YAxis.AxisDependency.LEFT);
+        set.setLineWidth(3);
+        set.setColor(Color.MAGENTA);
+        set.setMode(LineDataSet.Mode.CUBIC_BEZIER);
+        set.setCubicIntensity(0.2f);
+        return set;
+    }
+
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
 
@@ -124,19 +176,70 @@ public class DispRideStats_List extends AppCompatActivity implements SensorEvent
 
 
         if (rotationOK) {
-            SensorManager.getOrientation(rotationMatrix, orientations);
+
+            if(b_init == false)
+            {
+
+                SensorManager.getOrientation(rotationMatrix, orientations);
+                for (int i = 0; i < 3; i++) {
+                    init_values[i] = (float) (Math.toDegrees(orientations[i]));
+                }
+                if( init_values[0]!=0 && init_values[1]!= 0 && init_values[2]!=0 ){
+                    b_init = true;
+                }
+
+            }
+            else {
+                SensorManager.getOrientation(rotationMatrix, orientations);
+                for (int i = 0; i < 3; i++) {
+                    orientations[i] = (float) (Math.toDegrees(orientations[i]));
+                    float temp = orientations[i] - init_values[i];
+                    float temp_1 = Float.parseFloat(Angle_values[i]);
+                    Angles[i] = temp;
+                    datasets[i].addEntry(new Entry(datasets[i].getEntryCount(),temp));
+
+                    if(Math.abs(temp_1) < Math.abs(temp)) {
+                        Angle_values[i] = Float.toString(temp);
+                    }
+                    else{
+
+                    }
+                }
+                if(plotData == true) {
+                    lView = (ListView) findViewById(R.id.androidList);
+                    lAdapter = new ListAdapter(DispRideStats_List.this, Angle_Name, Angle_values, Angles, datasets, images);
+                    lView.setAdapter(lAdapter);
+                    plotData = true;
+                }
+            }
         }
-        for (int i = 0; i < 3; i++) {
-            orientations[i] = (float)(Math.toDegrees(orientations[i]));
-            float temp = orientations[i];
-            Angle_values[i] = Float.toString(temp);
+
+
+
+
+
+    }
+
+    private void startPlot(){
+
+        if(thread != null){
+            thread.interrupt();
         }
 
-
-        lView = (ListView) findViewById(R.id.androidList);
-        lAdapter = new ListAdapter(DispRideStats_List.this, Angle_Name, Angle_values, images);
-        lView.setAdapter(lAdapter);
-
+        thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true){
+                    plotData = true;
+                    try {
+                        Thread.sleep(100);
+                    }
+                    catch(InterruptedException e){
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
     }
 
     /**
@@ -146,4 +249,11 @@ public class DispRideStats_List extends AppCompatActivity implements SensorEvent
     @Override
     public void onAccuracyChanged(Sensor sensor, int i) {
     }
+
+    /*@Override
+    public void OnDestroy(){
+        mSensorManager.unregisterListener(DispRideStats_List.this);
+        thread.interrupt();
+        super.onDestroy();
+    }*/
 }
--- app/src/main/java/com/marichitech/ridestatistics/ListAdapter.java
+++ app/src/main/java/com/marichitech/ridestatistics/ListAdapter.java
@@ -1,8 +1,10 @@
 package com.marichitech.ridestatistics;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +12,16 @@ import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+import com.github.mikephil.charting.utils.Utils;
 
 import java.util.ArrayList;
 
@@ -20,13 +32,17 @@ public class ListAdapter extends BaseAdapter {
     private final String [] values;
     private final String [] numbers;
     private final int [] images;
+    private  final float [] angle_values;
+    private  final ILineDataSet [] datasets;
 
-    public ListAdapter(Context context, String [] values, String [] numbers, int [] images){
+    public ListAdapter(Context context, String [] values, String [] numbers, float [] angle_values,ILineDataSet[] datasets, int [] images){
         //super(context, R.layout.single_list_app_item, utilsArrayList);
         this.context = context;
         this.values = values;
         this.numbers = numbers;
         this.images = images;
+        this.angle_values = angle_values;
+        this.datasets = datasets;
     }
 
     @Override
@@ -48,8 +64,7 @@ public class ListAdapter extends BaseAdapter {
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
 
-
-        ViewHolder viewHolder;
+         ViewHolder viewHolder;
 
         final View result;
 
@@ -62,18 +77,64 @@ public class ListAdapter extends BaseAdapter {
             viewHolder.txtVersion = (TextView) convertView.findViewById(R.id.aVersiontxt);
             viewHolder.icon = (ImageView) convertView.findViewById(R.id.appIconIV);
 
+            viewHolder.chart2 = (LineChart) convertView.findViewById(R.id.chart1);
+            viewHolder.chart2.getDescription().setEnabled(true);
+            viewHolder.chart2.getDescription().setText("Real Time Data Plot");
+            viewHolder.chart2.setTouchEnabled(false);
+            viewHolder.chart2.setDragEnabled(false);
+            viewHolder.chart2.setScaleEnabled(true);
+            viewHolder.chart2.setPinchZoom(false);
+            viewHolder.chart2.setBackgroundColor(Color.WHITE);
+            LineData data = new LineData();
+            data.setValueTextColor(Color.WHITE);
+            viewHolder.chart2.setData(data);
+
+            Legend l = viewHolder.chart2.getLegend();
+            l.setForm(Legend.LegendForm.LINE);
+            l.setTextColor(Color.WHITE);
+            XAxis xl = viewHolder.chart2.getXAxis();
+            xl.setDrawGridLines(true);
+            xl.setTextColor(Color.WHITE);
+            xl.setAvoidFirstLastClipping(true);
+            xl.setEnabled(true);
+
+            YAxis leftAxis = viewHolder.chart2.getAxisLeft();
+            leftAxis.setTextColor(Color.WHITE);
+            leftAxis.setDrawGridLines(false);
+            leftAxis.setAxisMaximum(180f);
+            leftAxis.setAxisMinimum(-180f);
+            leftAxis.setDrawGridLines(true);
+
+            YAxis rightAxis = viewHolder.chart2.getAxisRight();
+            rightAxis.setEnabled(false);
+
+            viewHolder.chart2.getAxisLeft().setDrawGridLines(false);
+            viewHolder.chart2.getXAxis().setDrawGridLines(false);
+            viewHolder.chart2.setDrawBorders(false);
+
+
+            //startPlot();
             result=convertView;
 
             convertView.setTag(viewHolder);
         } else {
-            viewHolder = (ViewHolder) convertView.getTag();
-            result=convertView;
-        }
+            viewHolder = (ViewHolder)
+        convertView.getTag();
+        result=convertView;
+    }
 
         viewHolder.txtName.setText(values[position]);
         viewHolder.txtVersion.setText("Version: "+numbers[position]);
         viewHolder.icon.setImageResource(images[position]);
+        LineData data = viewHolder.chart2.getData();
+        if(datasets != null){
+            data.addDataSet(datasets[position]);
+        }
 
+        data.notifyDataChanged();
+        viewHolder.chart2.notifyDataSetChanged();
+        viewHolder.chart2.setMaxVisibleValueCount(150);
+        viewHolder.chart2.moveViewToX(data.getEntryCount());
         return convertView;
     }
 
@@ -82,6 +143,7 @@ public class ListAdapter extends BaseAdapter {
         TextView txtName;
         TextView txtVersion;
         ImageView icon;
+        LineChart chart2;
 
     }
 
